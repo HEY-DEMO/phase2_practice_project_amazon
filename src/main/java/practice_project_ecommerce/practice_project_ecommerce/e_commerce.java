1aef986f95127de0d815967f1ccc6c44f0e79252
Tharun <kodurutharun18@gmail.com>
2023-12-25 16:19:45 +0530

    some modifications in code

--- src/main/java/practice_project_ecommerce/practice_project_ecommerce/e_commerce.java
+++ src/main/java/practice_project_ecommerce/practice_project_ecommerce/e_commerce.java
@@ -1,8 +1,10 @@
 package practice_project_ecommerce.practice_project_ecommerce;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
 
 public class e_commerce {
 	static WebDriver driver;
@@ -15,6 +17,32 @@ public class e_commerce {
 		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		driver.get("https://www.amazon.in/");
-		
+		test1();
+		test2();
+		test3();
+		test4();
+	}
+	@Test
+	public static void test1() {
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("TV");
+		driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
+	}
+	@Test
+	public static void test2() {
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).clear();
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("mobile");
+		driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
+	}
+	@Test
+	public static void test3() {
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).clear();
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("watch");
+		driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
+	}
+	@Test
+	public static void test4() {
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).clear();
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("laptop");
+		driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
 	}
 }
